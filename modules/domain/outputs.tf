d4d40f1331fbee0e128e3154e101ce1f7f415b3a
Narongchai Thongkongkeaw <narongchai.t@socket9.com>
2023-12-02 17:10:06 +0700

    init commit

--- modules/domain/outputs.tf
+++ modules/domain/outputs.tf
@@ -1,7 +1,7 @@
-output "address" {
+/*output "address" {
   value       = data.external.address.result["address"]
   description = "IP Address of the node"
-}
+}*/
 
 output "name" {
   value       = proxmox_virtual_environment_vm.node.name
--- scripts/ip.sh
+++ scripts/ip.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-chmod 750 /usr/sbin/tcpdump
-chmod 750 /usr/bin/tcpdump
-
 MAX_RETRIES=30
 RETRY_INTERVAL=5
 IP_REGEX='^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
